e9d66d00dc82082cfb28b31b74700c18016e86f9
Joseph DALY <joseph300905@gmail.com>
2025-11-30 14:14:03 +0100

    use safer Zig wrappers around Linux API

--- src/main.zig
+++ src/main.zig
@@ -2,43 +2,21 @@ const std = @import("std");
 const graphite = @import("graphite");
 
 const c = @cImport({
-    @cInclude("string.h");
-    @cInclude("unistd.h");
-    @cInclude("arpa/inet.h");
-    @cInclude("netinet/in.h");
-    @cInclude("sys/socket.h");
-    @cInclude("errno.h");
     @cInclude("liburing.h");
 });
 
 const PORT = 25565;
 const ADDRESS = "127.0.0.1";
 
-const CError = error{
-    Err,
-};
-
-fn ctry(msg: [*:0]const u8, res: c_int) !c_int {
-    if (0 > res) {
-        const errno = c.__errno_location().*;
-        std.log.err("{s}: {s}", .{ msg, c.strerror(errno) });
-        return CError.Err;
-    }
-    return res;
-}
-
 pub fn main() !void {
-    const serverfd = try ctry("socket", c.socket(c.AF_INET, c.SOCK_STREAM, 0));
-    defer _ = c.close(serverfd);
+    const serverfd = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0);
+    defer std.posix.close(serverfd);
 
-    var sockaddr_in = c.sockaddr_in{
-        .sin_family = c.AF_INET,
-        .sin_port = c.htons(PORT),
-    };
-    _ = c.inet_pton(c.AF_INET, ADDRESS, &sockaddr_in.sin_addr);
+    const addr_in = try std.net.Address.parseIp4("127.0.0.1", 25565);
 
-    _ = try ctry("bind", c.bind(serverfd, @ptrCast(&sockaddr_in), @sizeOf(@TypeOf(sockaddr_in))));
-    _ = try ctry("listen", c.listen(serverfd, c.SOMAXCONN));
+    try std.posix.setsockopt(serverfd, std.posix.SOL.SOCKET, std.posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+    try std.posix.bind(serverfd, &addr_in.any, addr_in.getOsSockLen());
+    try std.posix.listen(serverfd, 128);
 
-    std.log.info("server listening on port: {d}", .{PORT});
+    std.log.info("server listening on port {d}.", .{PORT});
 }
