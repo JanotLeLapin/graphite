87eeffb6da4ac9256d15b47d3c7e087611fbab4e
Joseph DALY <joseph300905@gmail.com>
2025-11-30 13:40:48 +0100

    bind to port using Linux API

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,44 @@
 const std = @import("std");
 const graphite = @import("graphite");
 
+const c = @cImport({
+    @cInclude("string.h");
+    @cInclude("unistd.h");
+    @cInclude("arpa/inet.h");
+    @cInclude("netinet/in.h");
+    @cInclude("sys/socket.h");
+    @cInclude("errno.h");
+    @cInclude("liburing.h");
+});
+
+const PORT = 25565;
+const ADDRESS = "127.0.0.1";
+
+const CError = error{
+    Err,
+};
+
+fn ctry(msg: [*:0]const u8, res: c_int) !c_int {
+    if (0 > res) {
+        const errno = c.__errno_location().*;
+        std.log.err("{s}: {s}", .{ msg, c.strerror(errno) });
+        return CError.Err;
+    }
+    return res;
+}
+
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try graphite.bufferedPrint();
-}
+    const serverfd = try ctry("socket", c.socket(c.AF_INET, c.SOCK_STREAM, 0));
+    defer _ = c.close(serverfd);
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    var sockaddr_in = c.sockaddr_in{
+        .sin_family = c.AF_INET,
+        .sin_port = c.htons(PORT),
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    _ = c.inet_pton(c.AF_INET, ADDRESS, &sockaddr_in.sin_addr);
+
+    _ = try ctry("bind", c.bind(serverfd, @ptrCast(&sockaddr_in), @sizeOf(@TypeOf(sockaddr_in))));
+    _ = try ctry("listen", c.listen(serverfd, c.SOMAXCONN));
+
+    std.log.info("server listening on port: {d}", .{PORT});
 }
