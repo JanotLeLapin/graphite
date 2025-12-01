e13c0ea4a8ca4a096869b78c9f973b46e25f6985
Joseph DALY <joseph300905@gmail.com>
2025-12-01 08:00:32 +0100

    add client manager

--- src/main.zig
+++ src/main.zig
@@ -20,13 +20,81 @@ const Userdata = packed struct {
 };
 
 const Client = struct {
+    fd: i32,
     read_buf: [4096]u8,
     write_buf: [4096]u8,
     addr: std.os.linux.sockaddr,
 };
 
+const ClientManager = struct {
+    lookup: std.ArrayList(?*Client),
+    lookup_alloc: std.mem.Allocator,
+    client_alloc: std.mem.Allocator,
+
+    pub fn init(
+        initCap: usize,
+        lookup_alloc: std.mem.Allocator,
+        client_alloc: std.mem.Allocator,
+    ) !ClientManager {
+        return ClientManager{
+            .lookup = try std.ArrayList(?*Client).initCapacity(lookup_alloc, initCap),
+            .lookup_alloc = lookup_alloc,
+            .client_alloc = client_alloc,
+        };
+    }
+
+    pub fn deinit(self: *ClientManager) void {
+        self.lookup.deinit(self.lookup_alloc);
+    }
+
+    pub fn get(self: *ClientManager, fd: i32) ?*Client {
+        if (fd < 0) {
+            return null;
+        }
+
+        const ufd: usize = @intCast(fd);
+        if (ufd >= self.lookup.items.len) {
+            return null;
+        }
+
+        return self.lookup.items[ufd];
+    }
+
+    pub fn add(self: *ClientManager, fd: i32) !*Client {
+        const ufd: usize = @intCast(fd);
+        if (ufd >= self.lookup.items.len) {
+            try self.lookup.appendNTimes(self.lookup_alloc, null, ufd - self.lookup.items.len + 1);
+        }
+
+        const client = try self.client_alloc.create(Client);
+        client.fd = fd;
+        self.lookup.items[ufd] = client;
+
+        return client;
+    }
+
+    pub fn remove(self: *ClientManager, fd: i32) void {
+        if (fd < 0) {
+            return;
+        }
+
+        const ufd: usize = @intCast(fd);
+        if (self.lookup.items.len < fd) {
+            return;
+        }
+
+        if (self.lookup.items[ufd]) |conn| {
+            self.client_alloc.destroy(conn);
+            self.lookup.items[ufd] = null;
+        }
+    }
+};
+
 pub fn main() !void {
-    var clients = std.mem.zeroes([512]Client);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+    var client_manager = try ClientManager.init(8, gpa.allocator(), gpa.allocator());
+    defer client_manager.deinit();
 
     const serverfd = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0);
     defer std.posix.close(serverfd);
@@ -64,11 +132,8 @@ pub fn main() !void {
                 const cfd = cqe.res;
                 std.debug.print("new client: {d}\n", .{cfd});
 
-                clients[@intCast(cfd)] = Client{
-                    .read_buf = undefined,
-                    .write_buf = undefined,
-                    .addr = addr,
-                };
+                var client = try client_manager.add(cfd);
+                client.addr = addr;
 
                 {
                     const sqe = try ring.getSqe();
@@ -80,7 +145,7 @@ pub fn main() !void {
                 {
                     const sqe = try ring.getSqe();
                     sqe.opcode = std.os.linux.IORING_OP.READ;
-                    sqe.prep_read(cfd, &clients[@intCast(cfd)].read_buf, 0);
+                    sqe.prep_read(cfd, &client_manager.get(cfd).?.read_buf, 0);
                     sqe.user_data = @bitCast(Userdata{ .op = UserdataOp.Read, .d = 0, .fd = cfd });
                 }
 
@@ -91,16 +156,16 @@ pub fn main() !void {
                 const bytes: usize = @intCast(cqe.res);
 
                 if (0 == bytes) {
-                    clients[@intCast(cfd)] = std.mem.zeroes(Client);
+                    client_manager.remove(cfd);
                     std.debug.print("closed client: {d}\n", .{cfd});
                     continue;
                 }
 
-                std.debug.print("read {d} bytes from client {d}: {any}\n", .{ bytes, cfd, clients[@intCast(cfd)].read_buf[0..bytes] });
+                std.debug.print("read {d} bytes from client {d}: {any}\n", .{ bytes, cfd, client_manager.get(cfd).?.read_buf[0..bytes] });
 
                 const sqe = try ring.getSqe();
                 sqe.opcode = std.os.linux.IORING_OP.READ;
-                sqe.prep_read(cfd, &clients[@intCast(cfd)].read_buf, 0);
+                sqe.prep_read(cfd, &client_manager.get(cfd).?.read_buf, 0);
                 sqe.user_data = @bitCast(Userdata{ .op = UserdataOp.Read, .d = 0, .fd = cfd });
 
                 _ = try ring.submit();
