b7be6fb7c231eb42e14ccbb124d7fc0183492b39
Joseph DALY <joseph300905@gmail.com>
2025-12-09 22:07:40 +0100

    fix location encoding/decoding

--- protocol/src/types/location.zig
+++ protocol/src/types/location.zig
@@ -11,7 +11,7 @@ pub const Location = struct {
     };
 
     pub fn decode(buf: []const u8) !DecodedType {
-        const p: u64 = std.mem.readInt(u64, buf[0..8], .big);
+        const p: i64 = std.mem.readInt(i64, buf[0..8], .big);
         return DecodedType{
             .value = .{
                 .x = @intCast(p >> 38),
@@ -23,11 +23,11 @@ pub const Location = struct {
     }
 
     pub fn encode(value: common.chunk.Location, buf: []u8) !usize {
-        const x: u64 = @intCast(value.x);
-        const y: u64 = @intCast(value.y);
-        const z: u64 = @intCast(value.z);
+        const x: i64 = @intCast(value.x);
+        const y: i64 = @intCast(value.y);
+        const z: i64 = @intCast(value.z);
         const p = ((x & 0x3FFFFFF) << 38) | ((y & 0xFFF) << 26) | (z & 0x3FFFFFF);
-        std.mem.writeInt(u64, buf[0..8], p, .big);
+        std.mem.writeInt(i64, buf[0..8], p, .big);
         return 8;
     }
 };
