6adb1495d33757027d6e7c532e3163602a1ec3bf
Joseph DALY <joseph300905@gmail.com>
2025-12-13 19:26:35 +0100

    expose default module

--- core/src/module.zig
+++ core/src/module.zig
@@ -1,3 +1,5 @@
 pub const location = @import("module/location.zig");
+pub const default = @import("module/default.zig");
 
 pub const LocationModule = location.LocationModule;
+pub const DefaultModule = default.DefaultModule;
